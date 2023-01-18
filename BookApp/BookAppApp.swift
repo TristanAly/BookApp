753610322ac59f81c4a0db5e60509c62649d95d4
Tristan Aly <tristan.aly@gmail.com>
2023-01-18 14:12:46 +0100

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by apprenant1 on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
